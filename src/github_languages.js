f0b336914ffc4d06f9b0893ce2e71e1be44ea719
Eugen Morozov <26843480+meugenom@users.noreply.github.com>
2026-08-06 20:24:11 +0200

    remmoved a typo in the script and global object Info

--- src/github_languages.js
+++ src/github_languages.js
@@ -1,119 +1,92 @@
-/**
- * github fetcher
- */
-
 const axios = require("axios").default;
 
-let info = {
-  github: {
-    totalRepositories: 0,
-	allSize: 0,
-    languages: []
-    },
-  };
+const parseData = (resData) => {
+  if (resData.errors) {
+    throw new Error(`GitHub Languages GraphQL error: ${JSON.stringify(resData.errors)}`);
+  }
 
-const setData = async (res) => {
-  let object = JSON.parse(res)["data"];
-  info.github.totalRepositories = object["user"]["repositories"]["totalCount"];
-  let repNodes = object["user"]["repositories"]["nodes"];
-  let resultArray = [];
+  const user = resData.data?.user;
+  if (!user) {
+    throw new Error("GitHub Languages GraphQL returned empty user data");
+  }
 
-  //generate array type [{language: size}]
+  const repNodes = user.repositories.nodes;
+  const resultArray = [];
+
   repNodes.forEach((repNode) => {
-    const langNodes = repNode["languages"]["edges"];
+    const langNodes = repNode.languages.edges;
     langNodes.forEach((langNode) => {
-      //console.log(langNode["node"]["name"]+":"+langNode["size"])
-      const node = {};
-      node[langNode["node"]["name"]] = langNode["size"];
-      resultArray.push(node);
+      resultArray.push({ [langNode.node.name]: langNode.size });
     });
   });
-  //console.log(resultArray);
 
-  //sum by languages and sorting
-  var reducedArray = resultArray.reduce(function (accumulator, item) {
-    // loop over each item in the array
-    Object.keys(item).forEach(function (key) {
-      // loop over each key in the array item, and add its value to the accumulator.
-      // Don't forget to initialize the accumulator field if it's not
+  const reducedMap = resultArray.reduce((accumulator, item) => {
+    Object.keys(item).forEach((key) => {
       accumulator[key] = (accumulator[key] || 0) + item[key];
     });
     return accumulator;
   }, {});
-  //console.log(reducedArray);
 
-  //sorting
-  let sortable = [];
+  const sortable = [];
   let allSize = 0;
-  for (var language in reducedArray) {
-    sortable.push([language, reducedArray[language]]);
-	allSize = allSize + reducedArray[language];
+  for (const language in reducedMap) {
+    sortable.push([language, reducedMap[language]]);
+    allSize += reducedMap[language];
   }
 
-  sortable.sort(function (a, b) {
-    return b[1] - a[1];
-  });
-  //console.log(sortable);
+  sortable.sort((a, b) => b[1] - a[1]);
 
-  //need return first six languages with %
-  info.github.totalSize = allSize;
-  sortable.forEach((item)=>{
-	let tmp = {
-		name: item[0],
-		totalSize: item[1],
-		percentage:  Math.round((item[1]/allSize)*100 * 10)/10  
-	}	
-	info.github.languages.push(tmp);
-  })
-  //console.log(info.github.languages);
+  const languages = sortable.map((item) => ({
+    name: item[0],
+    totalSize: item[1],
+    percentage: Math.round((item[1] / allSize) * 100 * 10) / 10,
+  }));
+
+  return {
+    totalRepositories: user.repositories.totalCount,
+    totalSize: allSize,
+    languages,
+  };
 };
 
 const get = async (username, token) => {
-  let body = {
+  const body = {
     query: `
-			query {
-				user(login: "${username}") {
-					repositories(isFork: false, privacy: PUBLIC, ownerAffiliations: [OWNER], first: 100, orderBy: {field: NAME, direction: ASC}) {
-					  totalCount
-					  nodes {
-						name
-						owner {
-						  login
-						}
-						languages(first: 100, orderBy: {field: SIZE, direction: DESC}) {
-						  totalCount
-						  edges {
-							size
-							node {
-							  name
-							}
-						  }
-						}
-					  }
-					}
-				  }
-		  }
-		`,
+      query {
+        user(login: "${username}") {
+          repositories(isFork: false, privacy: PUBLIC, ownerAffiliations: [OWNER], first: 100, orderBy: {field: NAME, direction: ASC}) {
+            totalCount
+            nodes {
+              name
+              owner {
+                login
+              }
+              languages(first: 100, orderBy: {field: SIZE, direction: DESC}) {
+                totalCount
+                edges {
+                  size
+                  node {
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
     variables: {},
   };
-  let options = {
+
+  const options = {
     headers: {
       "Content-Type": "application/json",
       Authorization: `bearer ${token}`,
     },
   };
-  try {
-  await axios
-    .post("https://api.github.com/graphql", body, options)
-    .then(async (response) => {
-      //console.log(JSON.stringify(response.data));
-      setData(JSON.stringify(response.data));
-    });
-  } catch (error) {
-    console.error(error);
-  }
+
+  const response = await axios.post("https://api.github.com/graphql", body, options);
+  return parseData(response.data);
 };
 
-exports.get = get;
-exports.setData = setData;
-exports.info = info;
+module.exports = { get };
